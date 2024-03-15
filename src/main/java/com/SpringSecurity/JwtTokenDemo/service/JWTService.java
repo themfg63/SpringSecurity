79b52ae5ad12221d4bb1c3f7259064fcf78e6a15
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-03-15 11:23:22 +0300

    User Details and Jwt Service are creat

---
+++ src/main/java/com/SpringSecurity/JwtTokenDemo/service/JWTService.java
@@ -0,0 +1,4 @@
+package com.SpringSecurity.JwtTokenDemo.service;
+
+public interface JWTService {
+}
---
+++ src/main/java/com/SpringSecurity/JwtTokenDemo/service/impl/JWTServiceImpl.java
@@ -0,0 +1,43 @@
+package com.SpringSecurity.JwtTokenDemo.service.impl;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JWTServiceImpl {
+
+    private String generateToken(UserDetails userDetails){
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSiginKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims,T> claimsResolvers){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolvers.apply(claims);
+    }
+
+    private Key getSiginKey(){
+        byte[] key = Decoders.BASE64URL.decode("413F4428472B4B6250655368566D5970337336763979244226452948404D6351");
+        return Keys.hmacShaKeyFor(key);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder().setSigningKey(getSiginKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    public String extractUserName(String token){
+        return extractClaim(token,Claims::getSubject);
+    }
+}
