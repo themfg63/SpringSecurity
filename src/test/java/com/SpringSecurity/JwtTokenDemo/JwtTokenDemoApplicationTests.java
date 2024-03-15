edc33703689438dc743d97c8ec0b682d0d0ff461
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-03-15 10:28:50 +0300

    Project Setup,Database Connection and User Entity is created

---
+++ src/test/java/com/SpringSecurity/JwtTokenDemo/JwtTokenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurity.JwtTokenDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
